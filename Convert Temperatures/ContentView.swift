81e6d1f9fe30950a46deff808dcdad31079048ea
tanvirgeekios <tanvir.alam@iosventure.com>
2021-04-04 06:32:28 +0600

    Initial Commit

---
+++ Convert Temperatures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Convert Temperatures
+//
+//  Created by MD Tanvir Alam on 4/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Convert Temperatures/Convert_TemperaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Convert_TemperaturesApp.swift
+//  Convert Temperatures
+//
+//  Created by MD Tanvir Alam on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Convert_TemperaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
